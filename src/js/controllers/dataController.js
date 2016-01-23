a4160a5aa28f8c06500af797370bd31588e10a61
Kyle Neely <Kyle.Neely@inin.com>
2016-01-23 17:51:30 -0500

    wip

--- src/js/controllers/dataController.js
+++ src/js/controllers/dataController.js
@@ -5,7 +5,7 @@ angular.module('RDash')
     .controller('Hello', ["$scope", '$http', function ($scope, $http) {
         $scope.getInvoices = function () {
 
-            $http.get('http://localhost:8080/invoices').
+            $http.get('http://52.91.246.101:8081/invoices').
                 success(function (data) {
                     console.log(data);
                     $scope.invoices = data;
@@ -17,7 +17,7 @@ angular.module('RDash')
 
         $scope.createInvoice = function (invoice) {
 
-            url = 'http://localhost:8080/invoices';
+            url = 'http://52.91.246.101:8081/invoices';
             $http.post(url, invoice).success(function (inv) {
                 console.log("Successfully create invoice with id: " + inv.invoiceId)
             }).error(function (err) {
@@ -27,11 +27,21 @@ angular.module('RDash')
         }
 
         $scope.updateInvoice = function (invoice) {
-            url = 'http://localhost:8080/invoices/' + invoice.invoiceNo;
+            url = 'http://52.91.246.101:8081/invoices/' + invoice.invoiceNo;
             $http.put(url, invoice).success(function (inv) {
                 console.log("Successfully updated invoice with id: " + inv.invoiceId)
             }).error(function (err) {
                 console.log("Could not update invoice");
+                console.log(err)
+            });
+        }
+
+        $scope.deleteInvoice = function (invoice) {
+            url = 'http://52.91.246.101:8081/invoices/' + invoice.invoiceNo;
+            $http.delete(url).success(function (inv) {
+                console.log("Successfully delete invoice with id: " + invoice.invoiceId)
+            }).error(function (err) {
+                console.log("Could not delete invoice");
                 console.log(err)
             });
         }
